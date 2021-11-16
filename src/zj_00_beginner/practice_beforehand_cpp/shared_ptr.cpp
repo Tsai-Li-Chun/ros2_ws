95d04524f6f2198176d8c4b802a50be083c64e3e
Tsai-Li-Chun <4a237051@stust.edu.tw>
2021-11-16 13:20:18 +0800

    add zj_00/practice_beforehand_cpp

--- src/zj_00_beginner/practice_beforehand_cpp/shared_ptr.cpp
+++ src/zj_00_beginner/practice_beforehand_cpp/shared_ptr.cpp
@@ -1,28 +1,22 @@
 #include<memory>
 #include<iostream>
 
-struct A;
-struct B;
-struct A
-{  
-    std::weak_ptr<B> pointer;  
-    ~A()
-    {  
-        std::cout << "A 被銷燬" << std::endl;  
-    }  
-};  
-struct B
-{  
-    std::weak_ptr<A> pointer;  
-    ~B()
-    {  
-        std::cout << "B 被銷燬" << std::endl;  
-    }  
-};  
+class MyClass 
+{
+public:
+    MyClass()   { std::cout << "Constructor" << std::endl; }
+    ~MyClass()  { std::cout << "Destructor" << std::endl; }
+};
+
 int main()
 {
-    auto a = std::make_shared<A>();  
-    auto b = std::make_shared<B>();  
-    a->pointer = b;  
-    b->pointer = a;  
+    std::shared_ptr<MyClass> p1(new MyClass());
+
+    std::cout << p1.use_count() << std::endl;
+
+    std::shared_ptr<MyClass> p2(p1);
+    std::cout << p1.use_count() << ", " << p2.use_count() << std::endl;
+
+    std::shared_ptr<MyClass> p3(std::move(p1));
+    std::cout << p1.use_count() << ", " << p2.use_count() << std::endl;
 }
